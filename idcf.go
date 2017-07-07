f4099d157c73892fc037bf2b38d974426608ace1
akaishi <galigalikun@gmail.com>
2017-07-07 18:01:16 +0900

    init

---
+++ idcf.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+type Idcf struct {
+	ApiKey     string `json:"api_key"`
+	DeletePath string `json:"delete_path"`
+	Expired    int64  `json:"expired"`
+	secretKey  string
+	method     string
+	uri        string
+}
+
+func (i *Idcf) url() string {
+	return fmt.Sprintf("https://cdn.idcfcloud.com%s", i.uri)
+}
+
+func (i *Idcf) call(url string, expired time.Time) {
+	i.Expired = expired.Unix()
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	enc.Encode(&i)
+	req, _ := http.NewRequest(i.method, url, &buf)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("expired", fmt.Sprintf("%d", i.Expired))
+	req.Header.Set("signature", i.signature())
+
+	client := new(http.Client)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(byteArray))
+}
+
+func (i *Idcf) str() string {
+	request_body, err := json.Marshal(i)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("%s\n%s\n%s\n%d\n%s\n%s", i.method, i.ApiKey, i.secretKey, i.Expired, i.uri, string(request_body))
+}
+
+func (i *Idcf) signature() string {
+	mac := hmac.New(sha256.New, []byte(i.secretKey))
+	mac.Write([]byte(i.str()))
+
+	return base64.StdEncoding.EncodeToString([]byte(hex.EncodeToString(mac.Sum(nil))))
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
---
+++ idcf_test.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestSignature(t *testing.T) {
+	idcf := Idcf{
+		ApiKey:     "apikey",
+		method:     "DELETE",
+		DeletePath: "http://aaaaa/.*",
+		secretKey:  "secret",
+		Expired:    12345678,
+		uri:        "/api/v0/caches",
+	}
+
+	str := `DELETE
+apikey
+secret
+12345678
+/api/v0/caches
+{"api_key":"apikey","delete_path":"http://aaaaa/.*","expired":12345678}`
+
+	if idcf.str() != str {
+		t.Errorf("str:%s", idcf.str())
+	}
+
+	if idcf.signature() != "ODIwZDc0YTBhN2VmMmMyYzNkMGIwNGZlMzAwZGM1YmJjYzVkYmI5NzU0ZWE5NmZjZjkxNDg2NmE4NGFmYmIyMg==" {
+		t.Errorf("signature:%s", idcf.signature())
+	}
+}
+
+func TestCall(t *testing.T) {
+	bodyTest := `{"api_key":"apikey","delete_path":"http://aaaaa/.*","expired":1371646123}
+`
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("signature") != "ZDZmNDMyNWJjYzkyMmRiZmUwODk1ZGE2ZDFmZWI1NTI4ZGUwMmZlOGY4OWM1NWI5ZmRjNjhiN2QxYzFiNTUzZQ==" {
+			t.Errorf("signature:%s", r.Header.Get("signature"))
+		}
+		if r.Header.Get("expired") != "1371646123" {
+			t.Errorf("expired:%s", r.Header.Get("expired"))
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			t.Error(err)
+		}
+		if string(body) != bodyTest {
+			t.Errorf("body:%s", string(body))
+		}
+	}))
+
+	defer ts.Close()
+
+	idcf := Idcf{
+		ApiKey:     "apikey",
+		method:     "DELETE",
+		DeletePath: "http://aaaaa/.*",
+		secretKey:  "secret",
+		uri:        "/api/v0/caches",
+	}
+
+	idcf.call(ts.URL, time.Unix(1371646123, 0))
+}
